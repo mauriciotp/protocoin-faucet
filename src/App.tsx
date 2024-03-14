a524c571687aecf86baadebac207808d08f7c873
Mauricio Teixeira Pinto <mauricio.tp_@outlook.com>
2024-03-14 13:49:35 -0300

    feat: create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header className="mb-auto">
+          <div>
+            <h3 className="float-md-start mb-0">ProtoCoin faucet</h3>
+            <nav className="nav nav-masthead justify-content-center float-md-end">
+              <a
+                className="nav-link fw-bold py-1 px-0 active"
+                aria-current="page"
+                href="#"
+              >
+                Home
+              </a>
+              <a className="nav-link fw-bold py-1 px-0" href="#">
+                About
+              </a>
+            </nav>
+          </div>
+        </header>
+
+        <main className="px-3">
+          <h1>Get you ProtoCoins</h1>
+          <p className="lead">
+            Once a day, earn a 1.000 coins for free just connecting your
+            Metamask below.
+          </p>
+          <p className="lead">
+            <a
+              href="#"
+              className="btn btn-lg btn-light fw-bold border-white bg-white"
+            >
+              <img src="/assets/metamask.svg" alt="MetaMask logo" width={48} />
+              Connect MetaMask
+            </a>
+          </p>
+        </main>
+
+        <footer className="mt-auto text-white-50">
+          <p>
+            Built by{' '}
+            <a
+              href="https://github.com/mauriciotp"
+              className="text-white"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @mauriciotp
+            </a>
+            .
+          </p>
+        </footer>
+      </div>
+    </>
   );
 }
 
