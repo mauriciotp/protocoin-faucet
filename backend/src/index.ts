18ff69a03102feaf94142670f53390ce8deaa5a6
Mauricio Teixeira Pinto <mauricio.tp_@outlook.com>
2024-03-15 12:20:33 -0300

    feat: add web3

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import { mintAndTransfer } from './Web3Provider';
+
 import express, { Request, Response, NextFunction } from 'express';
 import morgan from 'morgan';
 
@@ -13,7 +15,12 @@ app.use(morgan('tiny'));
 app.post(
   '/mint/:wallet',
   async (req: Request, res: Response, next: NextFunction) => {
-    res.json(true);
+    try {
+      const tx = await mintAndTransfer(req.params.wallet);
+      res.json(tx);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
 );
 
