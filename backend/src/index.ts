992315aaefd37380f9cbb5f9ad92f29a33d83826
Mauricio <mauricio.tp_@outlook.com>
2024-03-16 12:27:56 -0300

    feat: prevent mint spam on backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,33 +5,40 @@ import { mintAndTransfer } from './Web3Provider';
 
 import express, { Request, Response, NextFunction } from 'express';
 import morgan from 'morgan';
-
-const PORT: number = parseInt(`${process.env.PORT || 3001}`);
+import cors from 'cors';
 
 const app = express();
 
 app.use(morgan('tiny'));
 
-import cors from 'cors';
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN || '*',
   })
 );
 
+const nextMint = new Map<string, number>();
+
 app.post(
   '/mint/:wallet',
   async (req: Request, res: Response, next: NextFunction) => {
+    const wallet = req.params.wallet;
+
+    if (nextMint.has(wallet) && nextMint.get(wallet)! > Date.now())
+      return res.status(400).json(`Try again tomorrow.`);
+
     try {
-      const tx = await mintAndTransfer(req.params.wallet);
+      const tx = await mintAndTransfer(wallet);
       res.json(tx);
     } catch (err: any) {
       console.log(err);
       res.status(500).json(err.message);
     }
+    nextMint.set(wallet, Date.now() + 1000 * 60 * 60 * 24);
   }
 );
 
+const PORT: number = parseInt(`${process.env.PORT || 3001}`);
 app.listen(PORT, () => {
   console.log('Server is listening at ' + PORT);
 });
